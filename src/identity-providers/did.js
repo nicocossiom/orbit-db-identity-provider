4a2f6cc39c842dbcc33e62d8f23d3a35f94c73c5
Nicolás Cossío Miravalles <nicocossiom@gmail.com>
2023-06-24 12:41:19 +0200

    added resolver option for did provider

--- src/identity-providers/did.js
+++ src/identity-providers/did.js
@@ -1,28 +1,28 @@
-import IdentityProvider from './interface.js'
-import * as u8a from 'uint8arrays'
 import { DID } from 'dids'
+import * as u8a from 'uint8arrays'
+import IdentityProvider from './interface.js'
 
 const TYPE = 'DID'
 
 class DIDIdentityProvider extends IdentityProvider {
-  constructor ({ didProvider }) {
+  constructor({ provider: didProvider, resolver: didResolver }) {
     super()
     this.did = new DID({
-      resolver: DIDIdentityProvider.did._resolver,
+      resolver: didResolver,
       provider: didProvider
     })
   }
 
-  static get type () {
+  static get type() {
     return TYPE
   }
 
-  async getId ({ space }) {
+  async getId({ space }) {
     if (!this.did.authenticated) await this.did.authenticate()
     return this.did.id
   }
 
-  async signIdentity (data, { space }) {
+  async signIdentity(data, { space }) {
     if (!this.did.authenticated) await this.did.authenticate()
     const payload = u8a.toString(u8a.fromString(data, 'base16'), 'base64url')
     const jws = await this.did.createJWS(payload)
@@ -30,7 +30,7 @@ class DIDIdentityProvider extends IdentityProvider {
     return `${jws.signatures[0].protected}..${jws.signatures[0].signature}`
   }
 
-  static setDIDResolver (resolver) {
+  static setDIDResolver(resolver) {
     if (!this.did) {
       this.did = new DID({ resolver })
     } else {
@@ -38,7 +38,7 @@ class DIDIdentityProvider extends IdentityProvider {
     }
   }
 
-  static async verifyIdentity (identity) {
+  static async verifyIdentity(identity) {
     if (!this.did) {
       throw new Error('The DID resolver must first be set with setDIDResolver()')
     }
